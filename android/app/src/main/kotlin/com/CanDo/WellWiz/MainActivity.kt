d5d2f08b3c3289f93af0b733ff40b2d024bc68a7
ppkekw <accplaceholder311@gmail.com>
2025-07-29 17:13:37 +0530

    package name changed

---
+++ android/app/src/main/kotlin/com/CanDo/WellWiz/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.CanDo.WellWiz
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity() 
--- android/app/src/main/kotlin/com/Sh1vT/WellnessWiz/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.Sh1vT.WellnessWiz
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
